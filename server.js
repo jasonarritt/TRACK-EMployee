fc9c5249dc6c8ddb5392c1d9fb7e1bc712d6b0ed
Jason Arritt <jsn.arritt@gmail.com>
2022-05-13 15:37:08 -0400

    Added base server.js structure

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const ctable = require("console.table");
 const express = require("express");
 const db = require("./db/connection");
 
@@ -7,6 +10,39 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// GIVEN a command-line application that accepts user input
+// WHEN I start the application
+// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+function mainMenu() {}
+
+// WHEN I choose to view all departments
+// THEN I am presented with a formatted table showing department names and department ids
+function viewAllDepartments() {}
+
+// WHEN I choose to view all roles
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+function viewAllRoles() {}
+
+// WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+function viewAllEmployees() {}
+
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
+function addDepartment() {}
+
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+function addRole() {}
+
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+function addEmployee() {}
+
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+function updateRole() {}
 
 // Start server after DB connection
 db.connect((err) => {
