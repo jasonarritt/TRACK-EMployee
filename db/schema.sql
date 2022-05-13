5ca60f0f156404acce5236b39e9143ea91458c6f
Jason Arritt <jsn.arritt@gmail.com>
2022-05-13 13:05:31 -0400

    Schema built and loads

--- db/schema.sql
+++ db/schema.sql
@@ -1,35 +1,37 @@
-DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE employees_db;
 
-
--- department
-
+-- Department table
+CREATE TABLE department (
 -- id: INT PRIMARY KEY
-
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
 -- name: VARCHAR(30) to hold department name
+  name VARCHAR(30) NOT NULL
+);
 
-
-
--- role
-
+-- Role table
+CREATE TABLE role (
 -- id: INT PRIMARY KEY
-
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
 -- title: VARCHAR(30) to hold role title
-
+  title VARCHAR(30) NOT NULL,
 -- salary: DECIMAL to hold role salary
-
+  salary DECIMAL(5,2) NOT NULL,
 -- department_id: INT to hold reference to department role belongs to
+  department_id INTEGER NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
 
-
-
--- employee
-
+-- Employee table
+CREATE TABLE employee (
 -- id: INT PRIMARY KEY
-
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
 -- first_name: VARCHAR(30) to hold employee first name
-
+  first_name VARCHAR(30) NOT NULL,
 -- last_name: VARCHAR(30) to hold employee last name
-
+  last_name VARCHAR(30) NOT NULL,
 -- role_id: INT to hold reference to employee role
-
+  role_id INTEGER NOT NULL,
 -- manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+  manager_id INTEGER NOT NULL,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
